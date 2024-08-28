f8959b2d1bbc1829db23c0f044de03ba3a04ed1c
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-08-28 16:40:38 +0800

    chore: remove unnecessary css files and added fonts and colors to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bakbak: ['Bakbak One', 'sans-serif'],
+        gothic: ['League Gothic', 'sans-serif'],
+        spartan: ['League Spartan', 'sans-serif'],
+      },
+      colors: {
+        yellow: {
+          DEFAULT: "#ffeb39",
+          50: "#fefde8",
+          100: "#fffec2",
+          200: "#fff889",
+          300: "#ffeb39",
+          400: "#fddc12",
+          500: "#ecc106",
+          600: "#cc9702",
+          700: "#a36b05",
+          800: "#86540d",
+          900: "#724511",
+          950: "#432405",
+
+        }
+      }
+    },
   },
   plugins: [],
 }
