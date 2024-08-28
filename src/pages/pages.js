3f3033e896a387cdb62205cccdcafe23e81b81bb
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-08-28 15:23:30 +0800

    chore: installed tailwindcss, shadcn, and routing

---
+++ src/pages/pages.js
@@ -0,0 +1,10 @@
+import LandingPage from "@src/pages/LandingPage";
+import NotFound from "@src/pages/NotFound";
+
+
+export const Pages = {
+    LandingPage,
+    NotFound
+}
+
+export default Pages;
