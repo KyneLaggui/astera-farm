ec08a89175d3fe8fda8954639f00104a82ce28c9
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-08-30 23:59:21 +0800

    fix: remove hover effect for mobile

--- src/components/navbar/MobileMenu.jsx
+++ src/components/navbar/MobileMenu.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const MobileMenu = ({ isMobileMenuOpen, toggleMobileMenu }) => {
   return (
     <div
-      className={`fixed inset-0 bg-black text-white transform transition-transform ${isMobileMenuOpen ? 'translate-x-0' : 'translate-x-full'} md:hidden`}
+      className={`z-10 fixed inset-0 bg-black text-yellow transform transition-transform ${isMobileMenuOpen ? 'translate-x-0' : 'translate-x-full'} md:hidden`}
     >
       <button
-        className="absolute top-4 right-4 text-white"
+        className="absolute top-4 right-4 text-yellow"
         onClick={toggleMobileMenu}
       >
         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -14,10 +14,10 @@ const MobileMenu = ({ isMobileMenuOpen, toggleMobileMenu }) => {
         </svg>
       </button>
       <div className="flex flex-col items-center mt-16 font-bakbak">
-        <a href="#produce" className="py-2 px-4 text-lg hover:bg-gray-700">Produce</a>
-        <a href="#tracking" className="py-2 px-4 text-lg hover:bg-gray-700">Tracking</a>
-        <a href="#about-us" className="py-2 px-4 text-lg hover:bg-gray-700">About Us</a>
-        <a href="#recommendations" className="py-2 px-4 text-lg hover:bg-gray-700">Recommendations</a>
+        <a href="#produce" className="py-2 px-4 text-lg">Produce</a>
+        <a href="#tracking" className="py-2 px-4 text-lg">Tracking</a>
+        <a href="#about-us" className="py-2 px-4 text-lg">About Us</a>
+        <a href="#recommendations" className="py-2 px-4 text-lg">Recommendations</a>
       </div>
     </div>
   );
