72c6743efb397f3578245bb32ca02ad51d66b965
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-08-30 23:58:56 +0800

    feat: added footer on every page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
 import './index.css'
 import Pages from '@src/pages/pages'
-import Navbar from './layouts/NavBar'
+import Navbar from '@src/layouts/NavBar'
+import Footer from '@src/layouts/Footer'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route index path="/produce" element={<Pages.Produce />} />
       <Route index path="/test" element={<Pages.TestPage />} />
     </Routes>
+    <Footer />
     </>
   )
 }
