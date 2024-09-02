5cf7f3021b6a8fa9b465f173777d0105668446cd
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-09-03 00:06:40 +0800

    feat: changing drawer and sheet depending on screen size

--- src/components/navbar/AddToCartSheet.jsx
+++ src/components/navbar/AddToCartSheet.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from '@src/components/ui/sheet';
 import { Button } from '@src/components/ui/button';
 import { ShoppingCart } from 'lucide-react';
@@ -9,6 +10,7 @@ import Vegetable2 from '@src/assets/images/Vegetable-2.png';
 import Vegetable3 from '@src/assets/images/Vegetable-3.png';
 import Vegetable4 from '@src/assets/images/Vegetable-4.png';
 import Vegetable5 from '@src/assets/images/Vegetable-5.png';
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from '@src/components/ui/drawer';
 
 const cartItems = [
   { image: Vegetable1, title: 'SUPERNOVA BETELGEUSE', amount: 149.00, quantity: 10 },
@@ -19,39 +21,78 @@ const cartItems = [
 ];
 
 const AddToCartSheet = () => {
+  const isDesktop = useMediaQuery({ query: "(min-width: 768px)" });
+
   return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <ShoppingCart className="h-6 cursor-pointer text-yellow" />
-      </SheetTrigger>
-      <SheetContent className="flex flex-col justify-between" onOpenAutoFocus={(e) => e.preventDefault()}>
-        <SheetHeader>
-          <SheetTitle>Add to Cart</SheetTitle>
-          <SheetDescription>
-            Add items to your cart with a click and review them before checkout.
-          </SheetDescription>
-        </SheetHeader>
-        <ScrollArea className="h-full">
-          <div className='flex flex-col gap-2'>
-            {cartItems.map((item, index) => (
-              <CartProducts
-                key={index}
-                image={item.image}
-                title={item.title}
-                amount={item.amount}
-                initialQuantity={item.quantity}
-              />
-            ))}
-          </div>
-        </ScrollArea>
-        <SheetFooter className="border-t pt-4">
-          <h1 className='text min-w-[100px] text-md font-medium'>Total Price <span className='text-yellow'>₱10,400</span></h1>
-          <SheetClose asChild>
-            <Button type="submit" className="w-full">Checkout</Button>
-          </SheetClose>
-        </SheetFooter>
-      </SheetContent>
-    </Sheet>
+    <>
+      {isDesktop ? (
+        <Sheet>
+          <SheetTrigger asChild>
+            <ShoppingCart className="h-6 cursor-pointer text-yellow" />
+          </SheetTrigger>
+          <SheetContent className="flex flex-col justify-between" onOpenAutoFocus={(e) => e.preventDefault()}>
+            <SheetHeader>
+              <SheetTitle>Add to Cart</SheetTitle>
+              <SheetDescription>
+                Add items to your cart with a click and review them before checkout.
+              </SheetDescription>
+            </SheetHeader>
+            <ScrollArea className="h-full">
+              <div className='flex flex-col gap-2'>
+                {cartItems.map((item, index) => (
+                  <CartProducts
+                    key={index}
+                    image={item.image}
+                    title={item.title}
+                    amount={item.amount}
+                    initialQuantity={item.quantity}
+                  />
+                ))}
+              </div>
+            </ScrollArea>
+            <SheetFooter className="border-t pt-4">
+              <h1 className='text min-w-[100px] text-md font-medium'>Total Price <span className='text-yellow'>₱10,400</span></h1>
+              <SheetClose asChild>
+                <Button type="submit" className="w-full">Checkout</Button>
+              </SheetClose>
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
+      ) : (
+        <Drawer>
+          <DrawerTrigger asChild>
+            <ShoppingCart className="h-6 cursor-pointer text-yellow" />
+          </DrawerTrigger>
+          <DrawerContent className="flex flex-col max-h-[90vh]" onOpenAutoFocus={(e) => e.preventDefault()}>
+            <DrawerHeader className="text-left">
+              <DrawerTitle>Add to Cart</DrawerTitle>
+              <DrawerDescription>
+                Add items to your cart with a click and review them before checkout.
+              </DrawerDescription>
+            </DrawerHeader>
+            <ScrollArea className="p-4 overflow-y-auto">
+              <div className='flex flex-col gap-2'>
+                {cartItems.map((item, index) => (
+                  <CartProducts
+                    key={index}
+                    image={item.image}
+                    title={item.title}
+                    amount={item.amount}
+                    initialQuantity={item.quantity}
+                  />
+                ))}
+              </div>
+            </ScrollArea>
+            <DrawerFooter className="border-t pt-4">
+              <h1 className='text min-w-[100px] text-md font-medium'>Total Price <span className='text-yellow'>₱10,400</span></h1>
+              <DrawerClose asChild>
+                <Button type="submit" className="w-full">Checkout</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      )}
+    </>
   );
 };
 
