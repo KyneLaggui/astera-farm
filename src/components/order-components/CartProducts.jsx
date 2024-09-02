448349321d16d6fad488141350a87f3f46180685
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-09-03 00:30:19 +0800

    style: spacing and padding depending on screen size on cart products

--- src/components/order-components/CartProducts.jsx
+++ src/components/order-components/CartProducts.jsx
@@ -22,29 +22,30 @@ const CartProducts = ({ image, title, amount, initialQuantity }) => {
   return (
     <Card>  
       <CardContent className="p-3">
-        <div className='flex justify-between'>
+        <div className='flex justify-between gap-2'>
 
-            <div className='flex items-center max-w-[240px] gap-2'>
+            <div className='flex items-center md:max-w-[240px] w-full d:gap-2 gap-4 max-w-[500px]'>
                 <div className='w-[120px] h-[120px] flex-shrink-0'>
                     <img src={image} alt='vegetable-pic' className='w-full h-full object-cover'/>
                 </div>
-                <div className='flex flex-col justify-start gap-4 max-w[150px] min-w-[150px]'>
-                    <div className='flex flex-col gap-2'>
-                        <h1 className='text-xl font-semibold max-h-[3.6em] overflow-hidden text-ellipsis line-clamp text-container'>
+                <div className='flex flex-col sm:gap-7 md:gap-2 gap-2 w-full md:max-w[150px] md:min-w-[150px]'>
+                    <h1 className='text-xl font-semibold max-h-[3.6em] overflow-hidden text-ellipsis line-clamp text-container'>
                         {title}
-                        </h1>
-                        <p className='text-md text-yellow'>₱{Number(amount) * Number(quantity)}</p>
+                    </h1>
+                    <div className='flex flex-col sm:flex-row sm:gap-7 md:flex-col md:gap-4 gap-4 items-start sm:items-center md:items-start'>
+                      <p className='text-md text-yellow'>₱{Number(amount) * Number(quantity)}</p>
+                      <div className='flex justify-between items-center border max-h-[40px] max-w-[200px] rounded-full px-2 py-1 w-full'>
+                          <Plus size={16} onClick={handleIncrement} className='cursor-pointer' />
+                          <Input 
+                          type="number" 
+                          value={quantity} 
+                          readOnly 
+                          className="w-9 h-7 p-0 pl-2 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                          />
+                          <Minus size={16} onClick={handleDecrement} className='cursor-pointer' />
+                      </div>
                     </div>
-                    <div className='flex justify-between items-center border rounded-full px-2 py-1 w-full'>
-                        <Plus size={16} onClick={handleIncrement} className='cursor-pointer' />
-                        <Input 
-                        type="number" 
-                        value={quantity} 
-                        readOnly 
-                        className="w-9 h-7 p-0 pl-2 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-                        />
-                        <Minus size={16} onClick={handleDecrement} className='cursor-pointer' />
-                    </div>
+                    
                 </div>
             </div>
 
