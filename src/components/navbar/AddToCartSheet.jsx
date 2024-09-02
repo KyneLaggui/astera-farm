c66c513d8976f8ab63a08c7caf77f3cc4a3cbb45
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-09-02 23:18:12 +0800

    chore: add more data values

--- src/components/navbar/AddToCartSheet.jsx
+++ src/components/navbar/AddToCartSheet.jsx
@@ -5,13 +5,17 @@ import { ShoppingCart } from 'lucide-react';
 import CartProducts from '@src/components/order-components/CartProducts';
 import { ScrollArea } from '@src/components/ui/scroll-area';
 import Vegetable1 from '@src/assets/images/Vegetable-1.png';
+import Vegetable2 from '@src/assets/images/Vegetable-2.png';
+import Vegetable3 from '@src/assets/images/Vegetable-3.png';
+import Vegetable4 from '@src/assets/images/Vegetable-4.png';
+import Vegetable5 from '@src/assets/images/Vegetable-5.png';
 
 const cartItems = [
   { image: Vegetable1, title: 'SUPERNOVA BETELGEUSE', amount: 149.00, quantity: 10 },
-  { image: Vegetable1, title: 'CRYSTAL LETTUCE', amount: 99.00, quantity: 5 },
-  { image: Vegetable1, title: 'GALACTIC SPINACH', amount: 79.00, quantity: 8 },
-  { image: Vegetable1, title: 'COSMIC KALE', amount: 129.00, quantity: 3 },
-  { image: Vegetable1, title: 'LEON ADRIEL FRANCO AGCAOILI ADBAJWDBAJD', amount: 159.00, quantity: 7 },
+  { image: Vegetable2, title: 'CRYSTAL LETTUCE', amount: 99.00, quantity: 5 },
+  { image: Vegetable3, title: 'GALACTIC SPINACH', amount: 79.00, quantity: 8 },
+  { image: Vegetable4, title: 'COSMIC KALE', amount: 129.00, quantity: 3 },
+  { image: Vegetable5, title: 'LEON ADRIEL FRANCO AGCAOILI ADBAJWDBAJD', amount: 159.00, quantity: 7 },
 ];
 
 const AddToCartSheet = () => {
