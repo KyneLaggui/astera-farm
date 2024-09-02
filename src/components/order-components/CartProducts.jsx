8f52263322fd91cecfa27c23bb2e594cc63b22ab
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-09-02 22:55:32 +0800

    feat: added increment and decrement, made it dynamic, adjust the styling with truncate

--- src/components/order-components/CartProducts.jsx
+++ src/components/order-components/CartProducts.jsx
@@ -1,34 +1,55 @@
-import React from 'react'
-import { Card, CardContent } from '@src/components/ui/card'
-import { CircleMinus, Minus, Plus } from 'lucide-react'
-import { Input } from '@src/components/ui/input'
-import Vegetable1 from '@src/assets/images/Vegetable-1.png'
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Card, CardContent } from '@src/components/ui/card';
+import { CircleMinus, Minus, Plus } from 'lucide-react';
+import { Input } from '@src/components/ui/input';
 
-const CartProducts = () => {
+const CartProducts = ({ image, title, amount, initialQuantity }) => {
+  const [quantity, setQuantity] = useState(initialQuantity);
+
+  useEffect(() => {
+    console.log('Initial Quantity:', initialQuantity);
+    setQuantity(initialQuantity);
+  }, [initialQuantity]);
+
+  const handleIncrement = () => {
+    setQuantity(prevQuantity => prevQuantity + 1);
+  };
+
+  const handleDecrement = () => {
+    setQuantity(prevQuantity => (prevQuantity > 0 ? prevQuantity - 1 : 0));
+  };
+
   return (
     <Card>  
-        <CardContent className="p-3">
-            <div className='flex justify-center gap-2'>
-            <div className='flex items-center'>
-                <img src={Vegetable1} alt='vegetable-pic' className='w-[130px]'/>
-                <div className='flex flex-col justify-start flex-end gap-4'>
-                    <div className='flex flex-col gap-2'>
-                        <h1 className='text-xl font-semibold'>SUPERNOVA BETELGEUSE</h1>
-                        <p className='text-md text-yellow'>₱149.00</p>
-                    </div>
-                    
-                    <div className='flex justify-evenly items-center border rounded-full px-2 py-1'>
-                        <Plus size={16} />
-                        <Input type="number" value="10" className="w-9 h-7 p-0 pl-2 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"/>
-                        <Minus size={16}  />
-                    </div>
-                </div>
+      <CardContent className="p-3">
+        <div className='flex justify-center gap-2 '>
+          <div className='flex items-center max-w-[280px]'>
+            <img src={image} alt='vegetable-pic' className='w-[130px]'/>
+            <div className='flex flex-col justify-start flex-end gap-4'>
+              <div className='flex flex-col gap-2'>
+                <h1 className='text-xl font-semibold max-h-[3.6em] overflow-hidden text-ellipsis line-clamp'>{title}</h1>
+                <p className='text-md text-yellow'>₱{Number(amount) * Number(quantity)}</p>
+              </div>
+              <div className='flex justify-evenly items-center border rounded-full px-2 py-1'>
+                <Plus size={16} onClick={handleIncrement} className='cursor-pointer' />
+                <Input type="number" value={quantity} readOnly className="w-9 h-7 p-0 pl-2 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"/>
+                <Minus size={16} onClick={handleDecrement} className='cursor-pointer' />
+              </div>
             </div>
-            <CircleMinus  className='h-6 w-6' />
-            </div>
-        </CardContent>
+          </div>
+          <CircleMinus className='h-5 w-5 cursor-pointer'/>
+        </div>
+      </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default CartProducts
+CartProducts.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+  initialQuantity: PropTypes.number.isRequired,
+};
+
+export default CartProducts;
