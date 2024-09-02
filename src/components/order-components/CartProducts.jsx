6ab61bbde31da402bee7f61ad30d6b121a09b2fa
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-09-02 23:18:31 +0800

    style: spacing and paddings and ellipsis

--- src/components/order-components/CartProducts.jsx
+++ src/components/order-components/CartProducts.jsx
@@ -17,29 +17,41 @@ const CartProducts = ({ image, title, amount, initialQuantity }) => {
   };
 
   const handleDecrement = () => {
-    setQuantity(prevQuantity => (prevQuantity > 0 ? prevQuantity - 1 : 0));
+    setQuantity(prevQuantity => (prevQuantity > 1 ? prevQuantity - 1 : 1));
   };
 
   return (
     <Card>  
       <CardContent className="p-3">
-        <div className='flex justify-center gap-2 '>
-          <div className='flex items-center max-w-[280px]'>
-            <img src={image} alt='vegetable-pic' className='w-[130px]'/>
-            <div className='flex flex-col justify-start flex-end gap-4'>
-              <div className='flex flex-col gap-2'>
-                <h1 className='text-xl font-semibold max-h-[3.6em] overflow-hidden text-ellipsis line-clamp'>{title}</h1>
-                <p className='text-md text-yellow'>₱{Number(amount) * Number(quantity)}</p>
-              </div>
-              <div className='flex justify-evenly items-center border rounded-full px-2 py-1'>
-                <Plus size={16} onClick={handleIncrement} className='cursor-pointer' />
-                <Input type="number" value={quantity} readOnly className="w-9 h-7 p-0 pl-2 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"/>
-                <Minus size={16} onClick={handleDecrement} className='cursor-pointer' />
-              </div>
+        <div className='flex justify-between'>
+
+            <div className='flex items-center max-w-[240px] gap-2'>
+                <div className='w-[120px] h-[120px] flex-shrink-0'>
+                    <img src={image} alt='vegetable-pic' className='w-full h-full object-cover'/>
+                </div>
+                <div className='flex flex-col justify-start gap-4 max-w[150px] min-w-[150px]'>
+                    <div className='flex flex-col gap-2'>
+                        <h1 className='text-xl font-semibold max-h-[3.6em] overflow-hidden text-ellipsis line-clamp text-container'>
+                        {title}
+                        </h1>
+                        <p className='text-md text-yellow'>₱{Number(amount) * Number(quantity)}</p>
+                    </div>
+                    <div className='flex justify-between items-center border rounded-full px-2 py-1 w-full'>
+                        <Plus size={16} onClick={handleIncrement} className='cursor-pointer' />
+                        <Input 
+                        type="number" 
+                        value={quantity} 
+                        readOnly 
+                        className="w-9 h-7 p-0 pl-2 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                        />
+                        <Minus size={16} onClick={handleDecrement} className='cursor-pointer' />
+                    </div>
+                </div>
             </div>
-          </div>
-          <CircleMinus className='h-5 w-5 cursor-pointer'/>
-        </div>
+
+            <CircleMinus className='h-5 w-5 cursor-pointer'/>
+            </div>
+
       </CardContent>
     </Card>
   );
