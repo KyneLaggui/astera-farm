8fa2ddee8d87019051999b5784e9be8b946f574f
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-09-03 22:38:21 +0800

    style: adjust the footer in responsiveness

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -8,27 +8,30 @@ import { CircleChevronRight } from 'lucide-react'
 
 const Footer = () => {
   return (
-    <div className='w-full bg-[#ffe500] bottom-0 flex flex-col gap-4 px-2 items-center lg:flex-row lg:justify-around lg:items-stretch py-5 rounded-t-3xl'>
+    <div className='w-full bg-[#ffe500] bottom-0 flex flex-col gap-4 p-7 sm:p-5 items-center lg:flex-row lg:justify-around lg:items-stretch rounded-t-3xl'>
         
         {/* Left Side */}
-        <div className='flex lg:justify-between flex-col items-center gap-4 lg:items-start '>
-            <img src={Logo} alt='Logo' className='w-[200px] object-contain'/>
-            <div className='flex items-center justify-center gap-4 lg:justify-start w-full'>
-                <img src={Facebook} alt='Logo' className='w-[35px] object-contain'/>
-                <img src={Instagram} alt='Logo' className='w-[35px] object-contain'/>
+        <div className='flex w-full sm:w-fit lg:justify-between sm:flex-col flex-between items-center lg:items-start justify-start gap-4 '>
+            <img src={Logo} alt='Logo' className='w-[160px] sm:w-[200px] object-contain'/>
+            <div className='flex items-center justify-end sm:justify-center lg:justify-start gap-4 w-full'>
+                <img src={Facebook} alt='Logo' className='sm:w-[35px] w-[30px] object-contain'/>
+                <img src={Instagram} alt='Logo' className='sm:w-[35px] w-[30px] object-contain'/>
             </div>
         </div>
 
         {/* Right Side */}
-        <div className='flex gap-4 w-fit items-center lg:items-start flex-col '>
+        <div className='flex gap-4 w-full sm:w-fit items-center lg:items-start flex-col '>
             <div className='flex flex-col font-spartan text-green items-center lg:items-start'>
-                <h1 className='font-extrabold text-3xl text-center'>Subscribe to our news letter!</h1>
-                <p className='font-semibold text-xl text-center'>Stay updated for upcoming discounts, releases and more!</p>
+                <h1 className='font-extrabold text-2xl sm:text-3xl text-center'>Subscribe to our news letter!</h1>
+                <p className='font-semibold text-lg sm:text-xl text-center'>Stay updated for upcoming discounts, releases and more!</p>
             </div>
-            <div className='flex w-full'>
-                <input type='text' placeholder='example@email.com' className='w-full px-3 rounded-l-full border-none' />
+            <div className='flex w-full max-h-[40px] sm:max-h-full'>
+                <input type='text' placeholder='example@email.com' className='w-full px-3 rounded-l-full border-none' onFocus={(e) => {
+                    e.target.style.color = "#475900";
+                    e.target.style.outline = "none";
+                }} />
                 <div className='flex bg-green text-yellow gap-2 py-1 px-2 items-center rounded-r-full'>
-                    <h1 className='text-2xl font-spartan font-extrabold mt-1'>Subscribe</h1>
+                    <h1 className='sm:text-2xl text-xl font-spartan font-extrabold mt-1'>Subscribe</h1>
                     <CircleChevronRight size={30} />
                 </div>
             </div>
