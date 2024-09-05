419e48bec0833b457fbeda93973ae4ef3599f62b
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-09-05 17:37:21 +0800

    feat: added styling and functionality in the call us page

---
+++ src/components/recommendations/CallCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import CallGreenImg from "@src/assets/images/call-us-green.png";
+import CallImg from "@src/assets/images/call-us.png";
+
+const CallCard = ({ minutes, amount }) => {
+  return (
+    <div className="group bg-yellow text-green-950 hover:bg-green hover:text-white sm:w-fit w-full flex flex-col items-center gap-4 px-7 py-3 rounded-3xl cursor-pointer">
+      <h1 className="font-spartan text-2xl font-extrabold uppercase ">
+        {minutes} Minutes
+      </h1>
+      <img
+        src={CallGreenImg}
+        alt="Call image"
+        className="max-w-[120px] sm:max-w-[80px] block group-hover:hidden w-full"
+      />
+      <img
+        src={CallImg}
+        alt="Call image"
+        className="max-w-[120px] sm:max-w-[80px] hidden group-hover:block w-full"
+      />
+      <h1 className="font-spartan text-4xl font-extrabold uppercase">
+        ₱{amount}
+      </h1>
+    </div>
+  );
+};
+
+CallCard.propTypes = {
+  minutes: PropTypes.number.isRequired,
+  amount: PropTypes.number.isRequired,
+};
+
+export default CallCard;
--- src/pages/recommendations/CallUs.jsx
+++ src/pages/recommendations/CallUs.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import backgroundImage from "@src/assets/images/background-image.png";
+import CallImg from "@src/assets/images/call-us.png";
+import { CircleChevronLeft } from "lucide-react";
+import Health1 from "@src/assets/images/health-1.png";
+import Health2 from "@src/assets/images/health-2.png";
+import Health3 from "@src/assets/images/health-3.png";
+import EveryHealthCard from "@src/components/recommendations/EveryHealthCard";
+import { Link } from "react-router-dom";
+import CallCard from "@src/components/recommendations/CallCard";
 
 const CallUs = () => {
   return (
-    <div>CallUs</div>
-  )
-}
+    <div
+      className="bg-cover bg-center flex justify-center items-start navbar-spacing w-full"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="w-[1200px] flex flex-col gap-2 justify-center items-start p-4">
+        <Link
+          to="/recommendations"
+          className="flex gap-2 w-full items-center justify-start text-yellow text-base cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105"
+        >
+          <CircleChevronLeft size={20} className="" />
+          <h1 className="">Back to Recommendations</h1>
+        </Link>
+        <div className="flex flex-col gap-10">
+          <div className="flex flex-col justify-start items-start">
+            <div className="flex justify-start item-start">
+              <h1 className="font-gothic text-6xl sm:text-8xl md:text-9xl text-white tracking-wide leading-none">
+                BOOK A CALL
+              </h1>
+              <img
+                src={CallImg}
+                className="w-full max-w-[45px] sm:max-w-[70px] md:max-w-[95px] object-contain"
+              />
+            </div>
+            <p className="font-spartan font-semibold max-w-[500px] text-sm text-white tracking-widest text-start">
+              BOOK A CALL WITH ONE OF OUR EXPERTS NOW!
+            </p>
+          </div>
+          <div className="flex flex-col gap-6">
+            <h1 className="text-3xl sm:text-4xl font-spartan font-black">
+              CHOOSE YOUR PACKAGE
+            </h1>
+            <div className="flex flex-wrap justify-center gap-6">
+              <CallCard minutes={15} amount="149.00" />
+              <CallCard minutes={30} amount="249.00" />
+              <CallCard minutes={45} amount="349.00" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CallUs
+export default CallUs;
