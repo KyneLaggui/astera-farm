011710a34b9c9a47cc6cadb68348421d890191d3
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-09-06 17:07:35 +0800

    style: centered the content on admin products

--- src/pages/admin/Products.jsx
+++ src/pages/admin/Products.jsx
@@ -4,7 +4,7 @@ import { ScrollArea } from "@src/components/ui/scroll-area";
 
 const Products = () => {
   return (
-    <div className="flex flex-col sm:items-center gap-8 bg-black navbar-spacing">
+    <div className="flex flex-col sm:items-center justify-center gap-8 bg-black navbar-spacing">
       <h1 className="font-gothic text-7xl sm:text-9xl text-white text-center tracking-wide">
         PRODUCTS
       </h1>
