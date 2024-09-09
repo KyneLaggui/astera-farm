4696e5b7d68ee30cc91c3e5e7dbae85486b4e60b
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-09-09 17:53:02 +0800

    feat: newsletter feature on footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,20 +1,71 @@
-import React from 'react'
-import Logo from '@src/assets/images/main-logo-green.png'
-import Facebook from '@src/assets/images/Facebook.png'
-import Instagram from '@src/assets/images/Instagram.png'
-import { CircleChevronRight } from 'lucide-react'
+import Logo from "@src/assets/images/main-logo-green.png";
+import Facebook from "@src/assets/images/Facebook.png";
+import Instagram from "@src/assets/images/Instagram.png";
+import { CircleChevronRight } from "lucide-react";
+import emailjs from "emailjs-com";
+import { useState } from "react";
 
 const Footer = () => {
+  const [email, setEmail] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (!email || !email.includes("@")) {
+      alert("Please enter a valid email");
+      return;
+    }
+
+    const serviceID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+    const templateID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+    const userID = import.meta.env.VITE_EMAILJS_USER_ID;
+
+    const templateParams = {
+      user_email: email,
+      image1:
+        "https://drive.google.com/uc?id=1jjmrufpNzSg9nKWBD_9sq1tNNFgo223D",
+      image2:
+        "https://drive.google.com/uc?id=12CujsKYGcp2byUJKuNEQM3gAzEv2XX-z",
+      image3:
+        "https://drive.google.com/uc?id=1hRN-5hd4qdHvL_e47eqU8Imic_8WlgFj",
+      main_logo:
+        "https://drive.google.com/uc?id=13Tf4IXSv872fMa7cCYrP_njtyb7R2FKL",
+    };
+
+    emailjs
+      .send(serviceID, templateID, templateParams, userID)
+      .then((response) => {
+        alert("Subscription successful! Welcome email sent.");
+        setEmail(""); // Clear the input value
+      })
+      .catch((error) => {
+        alert("Failed to send email, please try again.");
+        console.error("Failed to send email:", error);
+      });
+  };
+
   return (
-    <div className='w-full bg-[#ffe500] flex flex-col gap-4 p-7 relative sm:p-5 items-center lg:flex-row lg:justify-around lg:items-stretch rounded-t-3xl'>        
-        {/* Left Side */}
-        <div className='flex w-full sm:w-fit lg:justify-between sm:flex-col flex-between items-center lg:items-start justify-start gap-4 '>
-            <img src={Logo} alt='Logo' className='w-[160px] sm:w-[200px] object-contain'/>
-            <div className='flex items-center justify-end sm:justify-center lg:justify-start gap-4 w-full'>
-                <img src={Facebook} alt='Facebook Logo' className='sm:w-[35px] w-[30px] object-contain'/>
-                <img src={Instagram} alt='Instagram Logo' className='sm:w-[35px] w-[30px] object-contain'/>
-            </div>
+    <div className="w-full bg-[#ffe500] flex flex-col gap-4 p-7 relative sm:p-5 items-center lg:flex-row lg:justify-around lg:items-stretch rounded-t-3xl">
+      {/* Left Side */}
+      <div className="flex w-full sm:w-fit lg:justify-between sm:flex-col flex-between items-center lg:items-start justify-start gap-4 ">
+        <img
+          src={Logo}
+          alt="Logo"
+          className="w-[160px] sm:w-[200px] object-contain"
+        />
+        <div className="flex items-center justify-end sm:justify-center lg:justify-start gap-4 w-full">
+          <img
+            src={Facebook}
+            alt="Facebook Logo"
+            className="sm:w-[35px] w-[30px] object-contain"
+          />
+          <img
+            src={Instagram}
+            alt="Instagram Logo"
+            className="sm:w-[35px] w-[30px] object-contain"
+          />
         </div>
+      </div>
 
       {/* Right Side */}
       <div className="flex gap-4 w-full sm:w-fit items-center lg:items-start flex-col ">
@@ -26,23 +77,31 @@ const Footer = () => {
             Stay updated for upcoming discounts, releases and more!
           </p>
         </div>
-        <div className="flex w-full max-h-[40px] sm:max-h-full">
+        <form
+          onSubmit={sendEmail}
+          className="flex w-full max-h-[40px] sm:max-h-full"
+        >
           <input
             type="text"
             placeholder="example@email.com"
+            value={email} // Controlled input
+            onChange={(e) => setEmail(e.target.value)}
             className="w-full px-3 rounded-l-full border-none"
             onFocus={(e) => {
               e.target.style.color = "#475900";
               e.target.style.outline = "none";
             }}
           />
-          <div className="flex bg-green text-yellow gap-2 py-1 px-2 items-center rounded-r-full">
+          <button
+            type="submit"
+            className="flex bg-green text-yellow hover:text-yellow hover:bg-black gap-2 py-1 px-2 items-center rounded-r-full"
+          >
             <h1 className="sm:text-2xl text-xl font-spartan font-extrabold mt-1">
               Subscribe
             </h1>
             <CircleChevronRight size={30} />
-          </div>
-        </div>
+          </button>
+        </form>
         <p className="font-spartan text-green leading-none text-center">
           @2023, Astera Farms™. All Rights Reserved Privacy Policy. Supplier
           Code of Conduct.
@@ -53,4 +112,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
