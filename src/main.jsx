bc4334ab1f49af750c706f64ffed83ceb7f8b7e9
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-09-11 11:34:50 +0800

    feat: added toast container

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './redux/store'
-import { ThemeProvider } from '@src/components/theme-provider.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { ThemeProvider } from "@src/components/theme-provider.tsx";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
           <App />
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+            toastStyle={{ backgroundColor: "black" }}
+          />
         </ThemeProvider>
       </Provider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
