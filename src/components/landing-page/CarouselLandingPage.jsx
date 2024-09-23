91fb3224f591328a3ba95119841dfb6a7994acf1
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-09-23 16:00:10 +0800

    feat: added border radius on card content on carousel

--- src/components/landing-page/CarouselLandingPage.jsx
+++ src/components/landing-page/CarouselLandingPage.jsx
@@ -28,10 +28,8 @@ export function CarouselLandingPage() {
             className="flex justify-center items-center w-full"
           >
             <div className="w-full h-auto">
-              <Card className="w-full">
+              <Card className="w-full rounded-lg">
                 <CardContent className="relative pb-[56.25%]">
-                  {" "}
-                  {/* This maintains the 16:9 ratio */}
                   <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
                     <span className="text-4xl font-semibold">{index + 1}</span>
                   </div>
