fe0a155f53fd554f15de099146181a5144e32699
Joseph Jason Buhain <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2024-10-08 00:45:51 +0800

    feat: creation of SlideShowEdit component

---
+++ src/pages/admin/SlideShowEdit.jsx
@@ -0,0 +1,17 @@
+import ImageUpload from "@src/components/admin/imageUpload/ImageUpload";
+import LoggedInOnly from "@src/layouts/LoggedInOnly";
+
+const SlideShowEdit = () => {
+  return (
+    <LoggedInOnly forAdmin={true} forUser={false}>
+      <div className="navbar-spacing flex flex-col items-center justify-center gap-4 sm:gap-8 max-w-6xl mx-auto">
+        <h1 className="font-gothic text-7xl sm:text-9xl text-center tracking-wide">
+          Slideshow Management 
+        </h1>
+        <ImageUpload />
+      </div>
+    </LoggedInOnly>
+  );
+};
+
+export default SlideShowEdit;
