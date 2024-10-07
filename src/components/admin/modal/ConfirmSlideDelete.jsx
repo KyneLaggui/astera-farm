944e9a8b484ba9d415f47d32b277b7a60ce9d6c4
KayDeev/ <92616644+KyneLaggui@users.noreply.github.com>
2024-10-07 15:13:37 +0800

    style: change the color of alert dialogs in product and slide delete

--- src/components/admin/modal/ConfirmSlideDelete.jsx
+++ src/components/admin/modal/ConfirmSlideDelete.jsx
@@ -1,72 +1,72 @@
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-  } from "@src/components/ui/alert-dialog";
-  import { supabase } from "@src/supabase/config";
-  import { useState } from "react";
-  import { toast } from "react-toastify";
-  
-  const ConfirmSlideDelete = ({
-    slotIndex,
-    images,
-    setImages,
-    isDeleteDialogOpen,
-    onDialogClose,
-  }) => {
-    const [isDeleting, setIsDeleting] = useState(false); // Add a state for disabling the button
-  
-    const handleDelete = async (index) => {
-      setIsDeleting(true); // Disable the button once clicked
-  
-      const fileUrl = images[index];
-      const fileName = fileUrl.split("/").pop().split("?")[0]; // Extract file name from the URL
-  
-      const { error: deleteError } = await supabase.storage
-        .from("slideshow")
-        .remove([`public/${fileName}`]);
-  
-      if (deleteError) {
-        toast.error("Error deleting slide!");
-        setIsDeleting(false); // Re-enable the button if there's an error
-        return;
-      } else {
-        toast.success("Slide deleted successfully!");
-      }
-  
-      setImages((prevImages) => prevImages.filter((_, i) => i !== index));
-      setIsDeleting(false); // Re-enable the button after successful deletion
-      onDialogClose(); // Close the dialog after deletion
-    };
-  
-    return (
-      <AlertDialog open={isDeleteDialogOpen} onOpenChange={onDialogClose}>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your
-              slide image and remove data from the servers.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel onClick={onDialogClose}>Cancel</AlertDialogCancel>
-            <AlertDialogAction
-              onClick={() => handleDelete(slotIndex)}
-              disabled={isDeleting} // Disable the button while deleting
-            >
-              {isDeleting ? "Deleting..." : "Continue"}
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    );
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@src/components/ui/alert-dialog";
+import { supabase } from "@src/supabase/config";
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+const ConfirmSlideDelete = ({
+  slotIndex,
+  images,
+  setImages,
+  isDeleteDialogOpen,
+  onDialogClose,
+}) => {
+  const [isDeleting, setIsDeleting] = useState(false); // Add a state for disabling the button
+
+  const handleDelete = async (index) => {
+    setIsDeleting(true); // Disable the button once clicked
+
+    const fileUrl = images[index];
+    const fileName = fileUrl.split("/").pop().split("?")[0]; // Extract file name from the URL
+
+    const { error: deleteError } = await supabase.storage
+      .from("slideshow")
+      .remove([`public/${fileName}`]);
+
+    if (deleteError) {
+      toast.error("Error deleting slide!");
+      setIsDeleting(false); // Re-enable the button if there's an error
+      return;
+    } else {
+      toast.success("Slide deleted successfully!");
+    }
+
+    setImages((prevImages) => prevImages.filter((_, i) => i !== index));
+    setIsDeleting(false); // Re-enable the button after successful deletion
+    onDialogClose(); // Close the dialog after deletion
   };
-  
-  export default ConfirmSlideDelete;
-  
+
+  return (
+    <AlertDialog open={isDeleteDialogOpen} onOpenChange={onDialogClose}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            slide image and remove data from the servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={onDialogClose}>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => handleDelete(slotIndex)}
+            className="bg-green hover:bg-green-950"
+            disabled={isDeleting} // Disable the button while deleting
+          >
+            {isDeleting ? "Deleting..." : "Continue"}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default ConfirmSlideDelete;
